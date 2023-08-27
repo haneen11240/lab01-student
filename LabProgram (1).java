ab33fb2a7c3a7d555fc30adf5243c3a6da0d01f0
haneen11240 <hjqasem@albany.edu>
2023-08-27 17:30:04 -0400

    first commit

---
+++ LabProgram (1).java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      /* Type your code here. */
+	   Scanner keyboard = new Scanner(System.in);
+	   int num = keyboard.nextInt();
+	   int [] arr = new int[num];
+	   for (int i = 0;i < num; i++)
+		   arr[i] = keyboard.nextInt();
+	   //Get first 2 values to be printed
+	   Arrays.sort(arr);
+	   System.out.println(arr[0] + " and " + arr[1]);
+			   
+   }
+}
